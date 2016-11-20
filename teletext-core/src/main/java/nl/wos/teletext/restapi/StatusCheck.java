364cf7e08aa2f2c57c32264acea30052dba68be9
Stefan Kruijt <stefankruijt1991@gmail.com>
2016-11-20 09:22:53 -0600

    Unused imports

--- teletext-core/src/main/java/nl/wos/teletext/restapi/StatusCheck.java
+++ teletext-core/src/main/java/nl/wos/teletext/restapi/StatusCheck.java
@@ -1,16 +1,12 @@
 package nl.wos.teletext.restapi;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.wos.teletext.components.PhecapConnector;
-import nl.wos.teletext.dao.BerichtDao;
-import nl.wos.teletext.models.Bericht;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
